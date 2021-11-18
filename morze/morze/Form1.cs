bc79c9a21086f13f90974364c449513ea66272b4
GinalZsolt <93581265+GinalZsolt@users.noreply.github.com>
2021-11-18 15:43:32 +0100

    elso

---
+++ morze/morze/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace morze
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string abc = "abcdefghijklmnopqrstuvwxyz1234567890!?.,";
+        private string[] kod =
+        {
+            ".-",
+            "-...",
+            "-.-.",
+            "-..",
+            ".",
+            "..-.",
+            "--.",
+            "....",
+            "..",
+            ".---",
+            "-.-",
+            ".-..",
+            "--",
+            "-.",
+            "---",
+            ".--.",
+            "--.-",
+            ".-.",
+            "...",
+            "-",
+            "..-",
+            "...-",
+            ".--",
+            "-..-",
+            "-.--",
+            "--..",
+            ".----",
+            "..---",
+            "...--",
+            "....-",
+            ".....",
+            "-....",
+            "--...",
+            "---..",
+            "----.",
+            "-----",
+            "--.--",
+            "..--..",
+            ".-.-.-",
+            "--..--"
+        };
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "";
+            string szoveg = richTextBox1.Text;
+            for (int i = 0; i < szoveg.Length; i++)
+            {
+                for (int g = 0; g < abc.Length; g++)
+                {
+                    if (szoveg[i]==abc[g])
+                    {
+                        richTextBox2.Text += kod[g];
+                    }
+                }
+            }
+        }
+    }
+}
